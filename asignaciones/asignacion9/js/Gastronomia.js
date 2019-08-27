30b2ee56d7656a4a620e5b4be56dbd938677702b
luiselmer714 <luiselmermachacaprado@gmail.com>
2019-08-27 09:02:47 -0400

    ns

--- asignaciones/asignacion9/js/Gastronomia.js
+++ asignaciones/asignacion9/js/Gastronomia.js
@@ -6,7 +6,7 @@
     
  
     var section = document.querySelector('section');
-    
+    var footer=document.querySelector('footer');
     var requestURL = 'https://luiselmer714.github.io/luiselmer/asignaciones/asignacion9/js/gastronomia.json';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
@@ -17,8 +17,9 @@
       var superHeroes = JSON.parse(superHeroesText);
       populateHeader(superHeroes);
       gastronomia(superHeroes);
+      titulo(superHeroes);
       masvisitados(superHeroes);
-
+      
     }
   
 
@@ -38,8 +39,7 @@
         var myH2 = document.createElement('h2');
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myList = document.createElement('ul');
+     
         myH2.textContent = heroes[i].name;
         myPara1.textContent = 'Descripcion: ' + heroes[i].Descripcion;
         myPara2.textContent = 'Age: ' + heroes[i].age;
@@ -53,4 +53,32 @@
       }
     }
 
+    
+    function titulo(jsonObj) {
+      var myH1 = document.createElement('h1');
+      myH1.textContent = jsonObj['sitiosmasvisitados'];
+    footer.appendChild(myH1);
+     
+      
+    }
 
+
+    function masvisitados(jsonObj) {
+      var heroes = jsonObj['SitiosVisitados'];
+      for(var i = 0; i < heroes.length; i++) {
+        var myArticle = document.createElement('article');
+        var myH2 = document.createElement('h2');
+        var myPara1 = document.createElement('p');
+      
+        myH2.textContent = heroes[i].name;
+        myPara1.textContent = 'Descripcion: ' + heroes[i].Descripcion;
+      
+      
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+       
+       
+        
+        footer.appendChild(myArticle);
+      }
+    }
