f98b68ce675b588846b701c8a76a0e14e6b7427a
luiselmer714 <luiselmermachacaprado@gmail.com>
2019-08-29 14:48:01 -0400

    prueba100

--- asignaciones/asignacion10/js/clima.js
+++ asignaciones/asignacion10/js/clima.js
@@ -8,7 +8,7 @@ var direction;
 // URL http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
 // APIKEY: ###################
 // Identificación de Preston: 5604473
-let apiURLstring ='https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=a2278530004e75557bc8ffb56725d097';
+let apiURLstring ='https://api.openweathermap.org/data/2.5/weather?id=3907580&units=imperial&APPID=a2278530004e75557bc8ffb56725d097';
 weatherRequest.open ('GET', apiURLstring, true);
 weatherRequest.send ();
 
@@ -17,18 +17,21 @@ weatherRequest.send ();
 
 
  weatherRequest.onload = function () {
+     
  let weatherData = JSON.parse (weatherRequest.responseText);
  console.log (weatherData);
  document.getElementById ('cc-temp'). innerHTML = weatherData.main.temp;
- temp = document.getElementById('temperature').innerHTML=weatherData.main.temp;
- loc = document.getElementById("location").innerHTML=weatherData.main.loc;
+ temp =weatherData.main.temp;
 
+ var valNum = parseInt(temp);
+ document.getElementById("temperature").innerHTML = (valNum-32) / 1.8;
+ loc = document.getElementById("location").innerHTML=weatherData.main.loc;
  icon = document.getElementById("icon");
  humidity = document.getElementById("humidity").innerHTML=weatherData.main.humidity;
  wind = document.getElementById("wind").innerHTML=weatherData.main.wind;
  direction = document.getElementById("direction").innerHTML=weatherData.main.direction;
 
-
+//trukc row
 
 
  }
