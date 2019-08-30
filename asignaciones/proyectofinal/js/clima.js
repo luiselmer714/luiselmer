1134206bce345e25c3214aafb03e9c6d8084ce84
luiselmer714 <luiselmermachacaprado@gmail.com>
2019-08-30 10:04:24 -0400

    jnxckxd

---
+++ asignaciones/proyectofinal/js/clima.js
@@ -0,0 +1,37 @@
+let weatherRequest = new XMLHttpRequest ();
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
+
+let apiURLstring ='https://api.openweathermap.org/data/2.5/weather?id=3907584&units=imperial&APPID=a2278530004e75557bc8ffb56725d097';
+weatherRequest.open ('GET', apiURLstring, true);
+weatherRequest.send ();
+
+
+
+
+
+ weatherRequest.onload = function () {
+     
+ let weatherData = JSON.parse (weatherRequest.responseText);
+ console.log (weatherData);
+ document.getElementById ('cc-temp'). innerHTML = weatherData.main.temp;
+ temp =weatherData.main.temp;
+ var valNum = parseInt(temp);
+ document.getElementById("temperature").innerHTML = (((valNum-32) / 1.8).toFixed(2));
+ loc = document.getElementById("location").innerHTML=weatherData.main.loc;
+ document.getElementById("icon").setAttribute("src", "http://openweathermap.org/img/w/" + img01 + ".png");
+
+ humidity = document.getElementById("humidity").innerHTML=weatherData.main.humidity;
+ wind = document.getElementById("wind").innerHTML=weatherData.main.wind;
+ direction = document.getElementById("direction").innerHTML=weatherData.main.direction;
+
+//trukc row
+
+
+ }
+
+
