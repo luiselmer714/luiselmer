8daea677c2aa3d32f29d15a1f089439ea3d55879
luiselmer714 <luiselmermachacaprado@gmail.com>
2019-08-29 21:38:30 -0400

    ok

--- asignaciones/asignacion10/js/clima.js
+++ asignaciones/asignacion10/js/clima.js
@@ -5,10 +5,8 @@ var icon;
 var humidity;
 var wind;
 var direction;
-// URL http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
-// APIKEY: ###################
-// Identificación de Preston: 5604473
-let apiURLstring ='https://api.openweathermap.org/data/2.5/weather?id=3907580&units=imperial&APPID=a2278530004e75557bc8ffb56725d097';
+
+let apiURLstring ='https://api.openweathermap.org/data/2.5/weather?id=3907584&units=imperial&APPID=a2278530004e75557bc8ffb56725d097';
 weatherRequest.open ('GET', apiURLstring, true);
 weatherRequest.send ();
 
@@ -22,7 +20,6 @@ weatherRequest.send ();
  console.log (weatherData);
  document.getElementById ('cc-temp'). innerHTML = weatherData.main.temp;
  temp =weatherData.main.temp;
-
  var valNum = parseInt(temp);
  document.getElementById("temperature").innerHTML = (((valNum-32) / 1.8).toFixed(2));
  loc = document.getElementById("location").innerHTML=weatherData.main.loc;
